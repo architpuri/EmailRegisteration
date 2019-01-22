4d78d77e44dde033e9593c10fdabeef22df01ad9
Archit Puri <architpuri.7@gmail.com>
2019-01-22 15:47:24 +0530

    Create CommonAPIConstants.java

---
+++ CommonAPIConstants.java
@@ -0,0 +1,11 @@
+public class CommonAPIConstants {
+	
+	public final static String BASE_URL="localhost:8080/";
+	
+	//Spring MAIL
+	public final static String SPRING_MAIL_HOST="smtp.mailtrap.io";//url to send email
+	public final static int SPRING_MAIL_PORT=2525;//get from api application
+	public final static	String SPRING_MAIL_USERNAME="";//get from api application
+	public final static	String SPRING_MAIL_PASSWORD="";//get from api application
+	public final static String EMAIL_FROM="";//put email here
+  }
